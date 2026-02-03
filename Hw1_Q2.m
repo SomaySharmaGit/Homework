10bd3e92666845327aa32bcb1f53d40106b850a7
Somay's Laptop <somay052407@gmail.com>
2026-02-03 10:59:39 -0500

    Added a print function to reduce repeated code

--- Hw1_Q2.m
+++ Hw1_Q2.m
@@ -2,10 +2,13 @@ clc, clearvars;
 
 x = exp(1);
 y = pi;
-fprintf('The value of x is %4.2f and the value of y is %4.2f\n', x, y);
+print(x,y);
 
 temp = x;
 x = y;
 y = temp;
+print(x,y)
 
+function print(x,y)
 fprintf('The value of x is %4.2f and the value of y is %4.2f\n', x, y);
+end
