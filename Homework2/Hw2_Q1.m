475dda37f63f3402ab23f42dbd9ad9d5c263b350
Somay's Laptop <somay052407@gmail.com>
2026-02-04 19:16:31 -0500

    Made a function that calculates distance

--- Homework2/Hw2_Q1.m
+++ Homework2/Hw2_Q1.m
@@ -1,2 +1,11 @@
 clc, clearvars
 
+x1 = 0; y1 = 0; x2 = pi; y2 = exp(1);
+distance(x1,y1,x2,y2);
+
+
+function distance(x1,y1,x2,y2)
+result = sqrt((x2-x1)^2 + (y2-y1)^2);
+formatspec = 'The distance between (%f, %f) and (%f, %f) is %f\n';
+fprintf(formatspec,x1,y1,x2,y2,result);
+end
